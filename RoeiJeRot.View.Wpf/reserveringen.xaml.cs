38d740466ac6c97589b74e1eb06959003e7bc9c5
Timon Post <timonpost@hotmail.nl>
2019-11-27 13:15:13 +0100

    changes

---
+++ RoeiJeRot.View.Wpf/reserveringen.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RoeiJeRot.View.Wpf
+{
+    public class ViewModel
+    {
+        public int BoatTypeId { get; set; }
+        public int Status { get; set; }
+        public bool Reserved { get; set; }
+    }
+
+    /// <summary>
+    /// Interaction logic for reserveringen.xaml
+    /// </summary>
+    public partial class reserveringen : Window
+    {
+        public ObservableCollection<ViewModel> Items { get; set; }
+
+        public reserveringen()
+        {
+            InitializeComponent();
+
+            Items.Add(new ViewModel() { BoatTypeId = 1, Status = 2, Reserved = true});
+
+            DeviceDataGrid.ItemsSource = Items;
+        }
+    }
+}
