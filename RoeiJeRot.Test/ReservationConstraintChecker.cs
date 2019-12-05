89601c481d3365d8f2e8ca001cdfa3aafdf78ac9
Ayrton-Taede Tromp <ayrton.taede@gmail.com>
2019-12-05 10:58:25 +0100

    Added offset to datetime strings

--- RoeiJeRot.Test/ReservationConstraintChecker.cs
+++ RoeiJeRot.Test/ReservationConstraintChecker.cs
@@ -7,12 +7,12 @@ namespace RoeiJeRot.Test
     [TestFixture]
     public class ReservationConstraintChecker
     {
-        [TestCase("2020-11-18 13:00", 120, true)]
-        [TestCase("2020-11-18 20:00", 120, false)]
-        [TestCase("2020-11-18 23:00", 120, false)]
-        [TestCase("2020-11-18 8:05", 120, true)]
-        [TestCase("2020-11-18 8:00", 120, false)]
-        [TestCase("2020-11-18 16:34", 120, false)]
+        [TestCase("2020-11-18 13:00+01:00", 120, true)]
+        [TestCase("2020-11-18 20:00+01:00", 120, false)]
+        [TestCase("2020-11-18 23:00+01:00", 120, false)]
+        [TestCase("2020-11-18 8:05+01:00", 120, true)]
+        [TestCase("2020-11-18 8:00+01:00", 120, false)]
+        [TestCase("2020-11-18 16:34+01:00", 120, false)]
         [Test]
         public void dayTimeCheckerTest(string date, int duration, bool expected)
         {
