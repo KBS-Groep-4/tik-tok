b68bff494b8be6965b1546a046f63489629b856b
Ayrton-Taede Tromp <ayrton.taede@gmail.com>
2019-12-06 12:14:04 +0100

    Added comments

--- RoeiJeRot.Logic/Helper/ReservationConstraints.cs
+++ RoeiJeRot.Logic/Helper/ReservationConstraints.cs
@@ -6,12 +6,15 @@ using static Innovative.SolarCalculator.SolarTimes;
 
 namespace RoeiJeRot.Logic.Helper
 {
-    public class ReservationConstraintsMsg
+    /// <summary>
+    /// A message with a boolean to indicate a reservation is valid and a string to indicate why or why not
+    /// </summary>
+    public class ReservationConstraintsMessage
     {
         public bool IsValid { get; private set; }
         public string Reason { get; private set; }
 
-        public ReservationConstraintsMsg(bool isValid, string reason)
+        public ReservationConstraintsMessage(bool isValid, string reason)
         {
             IsValid = isValid;
             Reason = reason;
@@ -20,15 +23,23 @@ namespace RoeiJeRot.Logic.Helper
 
     public static class ReservationConstraints
     {
-        public static ReservationConstraintsMsg IsValid(DateTime date, TimeSpan duration, IReservationService reservationService, int accId)
+        /// <summary>
+        /// A function to check if a reservation is valid
+        /// </summary>
+        /// <param name="date">Starting DateTime of reservation</param>
+        /// <param name="duration">Duration of the reservation</param>
+        /// <param name="reservationService">Reservation service to check if there are more than 2 reservations already</param>
+        /// <param name="accountId">The account to check</param>
+        /// <returns>A message to indicate if a reservation is valid with info why or why not</returns>
+        public static ReservationConstraintsMessage IsValid(DateTime date, TimeSpan duration, IReservationService reservationService, int accountId)
         {
-            if (date < DateTime.Now || date + duration < DateTime.Now) return new ReservationConstraintsMsg(false, "Reservatie is in het verleden");
-            if (duration > TimeSpan.FromHours(2)) return new ReservationConstraintsMsg(false, "Reservatie is te lang (max 2 uur)");
-            if (reservationService.GetFutureReservations(accId).Count >= 2) return new ReservationConstraintsMsg(false, "U heeft al teveel reservaties geplaatst voor de toekomst");
+            if (date < DateTime.Now || date + duration < DateTime.Now) return new ReservationConstraintsMessage(false, "Reservatie is in het verleden");
+            if (duration > TimeSpan.FromHours(2)) return new ReservationConstraintsMessage(false, "Reservatie is te lang (max 2 uur)");
+            if (reservationService.GetFutureReservations(accountId).Count >= 2) return new ReservationConstraintsMessage(false, "U heeft al teveel reservaties geplaatst voor de toekomst");
 
-            if (!DayChecker.IsDay(date, duration)) return new ReservationConstraintsMsg(false, "Reservaties kunnen alleen tijdens de dag geplaatst worden");
+            if (!DayChecker.IsDay(date, duration)) return new ReservationConstraintsMessage(false, "Reservaties kunnen alleen tijdens de dag geplaatst worden");
 
-            else return new ReservationConstraintsMsg(true, "All is fine");
+            return new ReservationConstraintsMessage(true, "All is fine");
         }
     }
 }
